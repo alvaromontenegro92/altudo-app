b0ef758b2bcf1703986687b9459ad17767c9b0ac
Álvaro Montenegro <alvaro.montenegro@outlook.com.br>
2021-07-03 13:20:33 -0300

    feat: create model for form request

---
+++ AltudoApplication/AltudoApplication/Models/FormRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltudoApplication.WebApp.Models
+{
+    public class FormRequest
+    {
+        public string URL { get; set; }
+    }
+}
