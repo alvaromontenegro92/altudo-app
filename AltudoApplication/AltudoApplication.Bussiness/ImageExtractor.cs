430fc559c5bf42afb10e73fc3c53f4e8c4061051
Álvaro Montenegro <alvaro.montenegro@outlook.com.br>
2021-07-04 16:47:45 -0300

    feat: refactor and clean the class

--- AltudoApplication/AltudoApplication.Bussiness/ImageExtractor.cs
+++ AltudoApplication/AltudoApplication.Bussiness/ImageExtractor.cs
@@ -7,46 +7,16 @@ namespace AltudoApplication.Business
 {
     public class ImageExtractor
     {
+        private const string _initialCharacter = "src=\"";
+        private const string _finalCharacter = "\"";
+        private string[] _imagesExtension = {".png", ".jpg", ".jpge", ".bmp", ".svg", ".tiff", ".gif", ".avif", ".exif", ".raw", ".webp" };
         public List<Uri> ExtractImagesFromWebSite(Uri url)
         {
             var sourceCode = GetSourceCodeFromWebSite(url.AbsoluteUri);
             var imagesExtension = GetImageExtensionsAllowed();
+            var imagesAddress = GetImageAddress(url, sourceCode, imagesExtension);
 
-            var initialCharacter = "src=\"";
-            var finalCharacter = "\"";
-            var initialPosition = 0;
-            var finalPosition = 0;
-            var lastPositionFound = 0;
-
-            var imagesFound = new List<Uri>();            
-            
-            while (1 == 1)
-            {
-                if (sourceCode.Contains(initialCharacter))
-                {
-                    initialPosition = sourceCode.IndexOf(initialCharacter, lastPositionFound);
-
-                    if (initialPosition.Equals(-1))
-                        break;
-                    else
-                        initialPosition += initialCharacter.Length;
-
-                    finalPosition = sourceCode.IndexOf(finalCharacter, initialPosition);
-                    lastPositionFound = finalPosition;
-
-                    var image = sourceCode.Substring(initialPosition, finalPosition - initialPosition);
-
-                    var fullPathImage = new Uri(url, image);
-
-                    var imageExtension = Path.GetExtension(fullPathImage.AbsoluteUri);
-
-                    if (imagesExtension.Contains(imageExtension)
-                        && !imagesFound.Contains(fullPathImage))
-                        imagesFound.Add(fullPathImage);
-                }
-            }
-
-            return imagesFound;
+            return imagesAddress;
         }
 
         private string GetSourceCodeFromWebSite(string url)
@@ -66,15 +36,50 @@ namespace AltudoApplication.Business
 
             return result;
         }
-
         private List<string> GetImageExtensionsAllowed()
         {
-            return new List<string>(new string[] { 
-                ".png", 
-                ".jpg", 
-                "jpge", 
-                ".bmp", 
-                ".svg" });
+            return new List<string>(_imagesExtension);
+        }
+
+        private List<Uri> GetImageAddress(Uri url, string sourceCode, List<string> imagesExtension)
+        {
+            var find = true;
+            var initialPosition = 0;
+            var finalPosition = 0;
+            var lastPositionFound = 0;
+            var imagesAddressFound = new List<Uri>();
+
+            while (find)
+            {
+                if (sourceCode.Contains(_initialCharacter))
+                {
+                    initialPosition = sourceCode.IndexOf(_initialCharacter, lastPositionFound);
+
+                    if (initialPosition.Equals(-1))
+                        break;
+                    else
+                        initialPosition += _initialCharacter.Length;
+
+                    finalPosition = sourceCode.IndexOf(_finalCharacter, initialPosition);
+
+                    if (finalPosition.Equals(-1))
+                        break;
+
+                    lastPositionFound = finalPosition;
+
+                    var image = sourceCode.Substring(initialPosition, finalPosition - initialPosition);
+
+                    var fullPathImage = new Uri(url, image);
+
+                    var imageExtension = Path.GetExtension(fullPathImage.AbsoluteUri);
+
+                    if (imagesExtension.Contains(imageExtension)
+                        && !imagesAddressFound.Contains(fullPathImage))
+                        imagesAddressFound.Add(fullPathImage);
+                }
+            }
+
+            return imagesAddressFound;
         }
     }
 }
